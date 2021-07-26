f899a9659dc0ef764c9a8eef15b1de93bef20738
David Sotto Mayor <ddsmmf@gmail.com>
2021-07-26 16:33:26 -0400

    Iniciando com mongoose e criando Esquema de Publication

---
+++ back-end/models/publication.js
@@ -0,0 +1,22 @@
+const mongoose, { Schema, Model } = require('mongoose');
+
+const publicationSchema = new Schema({
+    author: {
+        type: String,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+});
+
+module.exports = Model('publication', publicationSchema);
