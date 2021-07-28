27b8ce377e6c5b3ccdad3f24741670aaab066941
David Sotto Mayor <ddsmmf@gmail.com>
2021-07-28 16:30:23 -0400

    Criando middleware para gerenciamento de erros

--- back-end/app.js
+++ back-end/app.js
@@ -23,6 +23,21 @@ app.use((req, res, next) => {
 const publicationRoutes = require('./routes/publication');
 app.use(publicationRoutes);
 
+// Setando middleware de gerenciamento de erros
+app.use(( error, req, res, next ) => {
+
+    console.log(error);
+
+    const errorStatusCode = error.statusCode || 500;
+    const errorMessage = error.message
+
+    res.status(errorStatusCode).json({
+        message: errorMessage
+    });
+
+})
+
+
 const port = 8080;
 
 mongoose.connect('mongodb://localhost:27017', {
--- back-end/routes/publication.js
+++ back-end/routes/publication.js
@@ -3,49 +3,15 @@ const router = express.Router();
 
 const Publication = require('../models/publication');
 
-// const publications = [
-//     {
-//         id: 1,
-//         author: 'David Sotto Mayor',
-//         title: 'Hoje o dia está radiante!',
-//         text: 'Hoje eu estou muito feliz e queria compartilhar isso!'
-//     },
-//     {
-//         id: 2,
-//         author: 'João Ninguém',
-//         title: 'Meu carro novo!',
-//         text: 'Estou muito feliz, comprei um carro zero e vou dirigir agora mesmo... uhul!'
-//     },
-//     {
-//         id: 3,
-//         author: 'Sicrano da Silva',
-//         title: 'Não compre, adote!',
-//         text: 'Hoje eu resgatei um gatinho da rua, agora ele está mais seguro.'
-//     },
-//     {
-//         id: 4,
-//         author: 'Beltrano Souza',
-//         title: 'Estou apredendo JS',
-//         text: 'JavaScript é uma linguagem incrível! Estou aprendendo bastante e logo estarei escrevendo vários códigos.'
-//     },
-//     {
-//         id: 5,
-//         author: 'Fulano Anônimo',
-//         title: 'Estou aprendendo a hackear.',
-//         text: 'Hoje não sei muita coisa... mas, quando eu pegar a prática ninguém vai se safar!'
-//     }
-// ];
-
 router.get('/', (req, res, next) => {
 
     Publication.find()
     .then( publications => {
 
         if(!publications){
-            return res.status(404).json({
-                message: "Nenhuma publicação encontrada!",
-                error: err
-            })
+            const error = new Error('Nenhuma publicação encontrada!')
+            error.statusCode = 404;
+            throw error;
         }
 
         res.status(200).json({
@@ -54,10 +20,10 @@ router.get('/', (req, res, next) => {
         });
     })
     .catch( err => {
-        res.status(500).json({
-            message: "Ocorreu um erro interno! Tente novamente mais tarde.",
-            error: err
-        })
+        if(!err.statusCode){
+            err.statusCode = 500;
+        }
+        next(err);
     })
 });
 
@@ -67,7 +33,9 @@ router.get('/publicacoes/:pubId', (req, res, next) => {
     Publication.findById(pubId)
     .then( publication => {
         if(!publication){
-            // tratar erro
+            const error = new Error('Nenhuma publicação encontrada!');
+            error.statusCode = 404;
+            throw error;
         }
 
         res.status(200).json({
@@ -76,11 +44,12 @@ router.get('/publicacoes/:pubId', (req, res, next) => {
         })
     })
     .catch( err => {
-        res.status(500).json({
-            message: "Ocorreu um erro interno! Tente novamente mais tarde.",
-            error: err
-        })
+        if(!err.statusCode){
+            err.statusCode = 500;
+        }
+        next(err)
     })
+    
 
 })
 
@@ -101,10 +70,8 @@ router.post('/nova-publicacao', (req, res, next) => {
         })
     })
     .catch( err => {
-        res.status(500).json({
-            message: "Ocorreu um erro interno! Tente novamente mais tarde.",
-            error: err
-        })
+        err.statusCode = 422;
+        next(err);
     })
     
 
@@ -121,7 +88,9 @@ router.put('/publicacao/:pubId', (req, res, next) => {
     Publication.findByIdAndUpdate(pubId)
     .then( publication => {
         if(!publication){
-            // tratar erro
+            const error = new Error('Publicação não encontrada!');
+            error.statusCode = 404;
+            throw error;
         }
 
         publication.author = updatedAuthor;
@@ -138,10 +107,10 @@ router.put('/publicacao/:pubId', (req, res, next) => {
         })
     })
     .catch( err => {
-        res.status(500).json({
-            message: "Ocorreu um erro interno! Tente novamente mais tarde.",
-            error: err
-        })
+        if(!err.statusCode){
+            err.statusCode = 422;
+        }
+        next(err)
     })
 })
 
@@ -151,7 +120,9 @@ router.delete('/publicacao/:pubId', (req, res, next) => {
     Publication.findByIdAndDelete(pubId)
     .then( publication => {
         if(!publication){
-            // tratar erro
+            const error = new Error('Publicação não encontrada!');
+            error.statusCode = 404;
+            throw error;
         }
 
         res.status(200).json({
@@ -161,10 +132,10 @@ router.delete('/publicacao/:pubId', (req, res, next) => {
 
     })
     .catch( err => {
-        res.status(500).json({
-            message: "Ocorreu um erro interno! Tente novamente mais tarde.",
-            error: err
-        })
+        if(!err.statusCode){
+            err.statusCode = 422;
+        }
+        next(err)
     })
 
 })
